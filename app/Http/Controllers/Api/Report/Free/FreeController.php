7705b239572edc664bf9e03c49c5e7a7a1541b19
Sasha137 <ivanov94.94@yandex.ru>
2018-05-24 11:08:50 +0300

    reports

--- app/Http/Controllers/Api/Report/Free/FreeController.php
+++ app/Http/Controllers/Api/Report/Free/FreeController.php
@@ -4,31 +4,17 @@ namespace App\Http\Controllers\Api\Report\Free;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Report\Free\PersonalResource;
-use App\PersonalTime;
+use App\Personal;
+use App\ProjectCost;
 use DB;
 
 class FreeController extends Controller
 {
     public function personal()
     {
-        $personal = PersonalTime::select(
-            DB::raw('sum(worktime) as worktime'),
-            'projects.name',
-            'projects.project_id'
-        )
-            ->groupBy('tasks.project_id')
-            ->whereYear('personal_times.date', 2018)
-            ->join('tasks', function ($join) {
-                $join->on('tasks.task_id', '=', 'personal_times.task_id');
-            })
-            ->join('projects', function ($join) {
-                $join->on('projects.project_id', '=', 'tasks.project_id');
-            })
-            ->get();
-
-        dd($personal);
-
-        return PersonalResource::collection($personal)
-            ->additional(['success' => true]);
+//        $projectCost = ProjectCost::
+//
+//        return PersonalResource::collection($personal)
+//            ->additional(['success' => true]);
     }
 }
--- app/Http/Resources/ProjectCostResource.php
+++ app/Http/Resources/ProjectCostResource.php
@@ -23,4 +23,3 @@ class ProjectCostResource extends JsonResource
         ];
     }
 }
-+
