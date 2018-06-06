09c74e0b4f6f1dd5f323f48f3a95d88b959eee5f
Sasha137 <ivanov94.94@yandex.ru>
2018-06-06 13:03:04 +0300

    id project to vue component

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -40,24 +40,6 @@ class ProjectController extends Controller
      */
     public function show($id, Request $request)
     {
-//        $dates = ProjectCost::select('date')->where('project_id', $id)
-//            ->orderBy('date', 'desc')
-//            ->groupBy('date')
-//            ->get();
-//
-//        //Если есть дата делаем переменную для запроса
-//        if ($request->filled('date')) {
-//            $date = $request->date;
-//        } else {
-//            $date = $dates->first()->date ?? date('Y-m');
-//        }
-//
-//        $project = Project::where('project_id', $id)
-//            ->with(['costs' => function ($query) use ($date) {
-//                $query->whereMonth('year_month', $date)->with('pers');
-//            }])
-//            ->firstOrFail();
-//
-       return view('projects.show', compact('project', 'dates'));
+       return view('projects.show', compact('id'));
     }
 }
