20edc8c260fe6e292be9e5328735fbe14dce7bed
Sasha137 <ivanov94.94@yandex.ru>
2018-06-06 13:00:26 +0300

    edit route

--- routes/web.php
+++ routes/web.php
@@ -56,7 +56,7 @@ Route::group(['middleware' => 'auth'], function () {
     //Resource Report Summary
     Route::get('api/report/summary', 'Api\Report\SummaryController@index')->name('api.report.summary');
 
-    Route::get('api/report/projects/{id}', 'Api\Report\ProjectController@show')->name('api.report.projects.show');
+    Route::get('api/report/projects/{id}/hours-spent', 'Api\Report\ProjectController@show')->name('api.report.projects.show');
 
     //----------------------------------------------------------------------------------------------------------------//
 
