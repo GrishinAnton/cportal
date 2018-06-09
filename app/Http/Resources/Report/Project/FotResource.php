fbb9c287b2bd89361070989f29154369ac9c245b
Sasha137 <ivanov94.94@yandex.ru>
2018-06-09 11:40:52 +0300

    fix fot resource

--- app/Http/Resources/Report/Project/FotResource.php
+++ app/Http/Resources/Report/Project/FotResource.php
@@ -14,6 +14,11 @@ class FotResource extends JsonResource
      */
     public function toArray($request)
     {
-        return $this->resource;
+        return [
+            'pers_id' => $this->resource->pers_id,
+            'first_name' => $this->resource->first_name,
+            'last_name' => $this->resource->last_name,
+            'salaries' => collect($this->resource)->except(['first_name', 'last_name', 'pers_id']),
+        ];
     }
 }
--- app/Repositories/Project/CostRepository.php
+++ app/Repositories/Project/CostRepository.php
@@ -1,15 +1,76 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: alexandr
- * Date: 08.06.18
- * Time: 18:52
- */
 
 namespace App\Repositories\Project;
 
+use App\Personal;
+use DB;
 
-class CostRepository
+class CostRepository extends ProjectRepository
 {
+    public function costs()
+    {
+        $personal = Personal::select($this->getFotSelect())
+            ->rightJoin(DB::raw("(
+                SELECT "
+                . $this->getT3Select() .
+                "pers_id
+                FROM (
+                    SELECT "
+                . $this->getT2Select() .
+                "pers_id,
+                        task_id
+                    FROM (
+                        SELECT
+            	            CONCAT(YEAR(date), '-', MONTH(date)) as yearmonth,
+            	            pers_id,
+            	            worktime,
+            	            task_id
+                        FROM personal_times
+                        WHERE task_id IN (
+                            SELECT task_id FROM tasks WHERE project_id = {$this->projectId}
+                        )
+        	        ) as t2
+                ) as t3
+                GROUP BY t3.pers_id
+            ) as t4"), 't4.pers_id', '=', 'personal.pers_id')
+            ->leftJoin(DB::raw("(
+                SELECT "
+                . $this->getT3Select('z') .
+                "pers_id
+                FROM (
+                    SELECT "
+                . $this->getT2Select('z') .
+                "pers_id,
+                    task_id
+                    FROM (
+                        SELECT
+            	            CONCAT(YEAR(date), '-', MONTH(date)) as yearmonth,
+            	            pers_id,
+            	            worktime,
+            	            task_id
+                        FROM personal_times
+                    ) as t2
+                ) as t3
+                GROUP BY t3.pers_id
+            ) as t5"), 't5.pers_id', '=', 'personal.pers_id')
+            ->leftJoin(DB::raw("(
+                SELECT "
+                . $this->getT3Select('s') .
+                "personal.pers_id
+                FROM (
+                    SELECT "
+                . $this->getT2Select('s', 'cost') .
+                "pers_id
+                    FROM (
+                        SELECT
+            	            CONCAT(YEAR(date), '-', MONTH(date)) as yearmonth,
+            	            cost
+                        FROM costs
+                    ) as t2
+                ) as t3
+	            GROUP BY pers_id
+            ) as t6"), 't6.pers_id', '=', 'personal.pers_id');
 
+        return $personal;
+    }
 }
