5a9696cb9a3174ef9b49a84ffac8c8be3bb736ab
Grishin Anton <grishin_anton@mail.ru>
2018-06-28 15:41:41 +0300

    Добавил письмо для проектов

--- resources/views/emails/closed_time_project.blade.php
+++ resources/views/emails/closed_time_project.blade.php
@@ -1,68 +1,89 @@
 <table  align="center" cellspacing="0" cellpadding="0" border="0" style="width:100%; max-width:600px; border:0;">
     <tr>
-        <td style="width: 100%;padding-top:20px;padding-bottom:20px;text-align:center;"><img style="max-width: 130px;" src="{{ asset('images/logotype.jpg') }}"></td>        
+        <td style="width: 100%;padding-top:20px;padding-bottom:20px;text-align:center;"><img style="max-width: 130px;" src="{{ asset('images/logotype.png') }}"></td>        
     </tr>
      <tr>
-        <td style="width: 100%;padding-top:0px;padding-bottom:20px;text-align:center;font-family: Arial, Helvetica, sans-serif;font-size:20px; line-height:20px; color:#222222;">Выработка {Данные сотрудника}</td>        
+        <td style="width: 100%;padding-top:0px;padding-bottom:20px;text-align:center;font-family: Arial, Helvetica, sans-serif;font-size:20px; line-height:20px; color:#222222;">Данные по проекту {Название проекта}</td>        
     </tr>
     <tr>
-        <td style="width: 100%;padding-top:15px;padding-bottom:15px;text-align:center;font-family: Arial, Helvetica, sans-serif;font-size:16px; line-height:16px; color:#222222;"> Данные в часах по дням за последнюю неделю</td>        
+        <td style="width: 100%;padding-top:15px;padding-bottom:15px;text-align:center;font-family: Arial, Helvetica, sans-serif;font-size:16px; line-height:16px; color:#222222;">
+
+            <div style="display:inline-block;min-width: 200px;border: 1px solid grey;padding-left: 10px;padding-top:15px;padding-bottom:15px;margin-bottom: 15px;">
+            
+                <ul  style="list-style-type: none;padding:0;margin:0;">
+                    <li>
+                        <span>Старт: </span>
+                        <span> </span>
+                    </li>
+                    <li>
+                        <span>Финиш: </span>
+                        <span> </span>
+                    </li>
+                    <li>
+                        <span>Статус: </span>
+                        <span> </span>
+                    </li>
+                    <li>
+                        <span>Компания: </span>
+                        <span> </span>
+                    </li>
+                    <li>
+                        <span>Менеджер: </span>
+                        <span> </span>
+                    </li>
+                </ul>
+                <hr />
+                <ul  style="list-style-type: none;padding:0;margin:0;">
+                    <li>
+                        <span>Часов потрачено: </span>
+                        <span> </span>
+                    </li>
+                    <li>
+                        <span>Часов заложено: </span>
+                        <span> </span>
+                    </li>
+                    <li>
+                        <span>Стоимость часа: </span>
+                        <span> </span>
+                    </li>
+                </ul>
+                <hr />
+                <ul  style="list-style-type: none;padding:0;margin:0;">
+                    <li>
+                        <span>Бюджет: </span>
+                        <span> </span>
+                    </li>
+                    <li>
+                        <span>Затраты: </span>
+                        <span> </span>
+                    </li>
+                    <li>
+                        <span>Остаток: </span>
+                        <span> </span>
+                    </li>
+                    <li>
+                        <span>Средний расход в неделю: </span>
+                        <span> </span>
+                    </li>
+                    <li>
+                        <span>Осталось недель: </span>
+                        <span> </span>
+                    </li>
+                </ul>
+                <hr />
+                <ul  style="list-style-type: none;padding:0;margin:0;">
+                    <li>
+                        <span>ФОТ: </span>
+                        <span> </span>
+                    </li>
+                    <li>
+                        <span>Издержки: </span>
+                        <span> </span>
+                    </li>
+                </ul>
+            </div> 
+        
+        </td>        
     </tr>
 
-        <tr style="text-align:center;">
-            <td>
-                <span>Пн (10.06)</span>
-                <span> - </span>
-                <span>8ч</span>
-            </td>        
-        </tr>
-         
-        <tr style="text-align:center;">
-            <td>
-                <span>Пн (10.06)</span>
-                <span> - </span>
-                <span>8ч</span>
-            </td>        
-        </tr> 
-        <tr style="text-align:center;">
-            <td>
-                <span>Пн (10.06)</span>
-                <span> - </span>
-                <span>8ч</span>
-            </td>        
-        </tr> 
-
- 
-    <tr>
-        <td style="width: 100%;padding-top:15px;padding-bottom:15px;text-align:center;font-family: Arial, Helvetica, sans-serif;font-size:16px; line-height:16px; color:#222222;">Данные за последние недели месяца</td>        
-    </tr>
-    <tr style="text-align:center;">
-        <td>
-            <span>10.06 - 17.06</span>
-            <span> - </span>
-            <span>25ч.</span>
-        </td>        
-    </tr> 
-    <tr style="text-align:center;">
-        <td>
-            <span>10.06 - 17.06</span>
-            <span> - </span>
-            <span>25ч.</span>
-        </td>        
-    </tr> 
-    <tr>
-        <td style="width: 100%;padding-top:15px;padding-bottom:15px;text-align:center;font-family: Arial, Helvetica, sans-serif;font-size:16px; line-height:16px; color:#222222;">Данные за последние три месяца</td>        
-    </tr>
-    <tr style="text-align:center;">>
-        <td>
-            <span>Январь</span>
-            <span>120 ч.</span>
-        </td>     
-    </tr>  
-    <tr style="text-align:center;">>
-        <td>
-            <span>Февраль</span>
-            <span>120 ч.</span>
-        </td>     
-    </tr>   
 </table>
