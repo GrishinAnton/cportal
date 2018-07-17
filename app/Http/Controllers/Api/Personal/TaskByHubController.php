7a8dfe3044d870d4ea8c5344f46c7b8dd7adbd94
Coldbwinz <whentheendisgettingcloser@mail.ru>
2018-07-17 17:02:54 +0300

    load 2up v1

--- app/Http/Controllers/Api/Personal/TaskByHubController.php
+++ app/Http/Controllers/Api/Personal/TaskByHubController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers\Api\Personal;
 
-use App\Http\Requests\PersonalFilterRequest;
-use App\Http\Resources\CompanyGroupResource;
+use App\Console\Commands\Api\Tasks;
+use App\Http\Requests\PersonalBusyRequest;
+use App\Http\Resources\Personal\PersonalBusyResource;
 use App\Http\Resources\PersonalResource;
 use App\Http\Controllers\Controller;
 use App\Personal;
 use DB;
-use DateTime;
+use App\Task;
+use Carbon\Carbon;
 
 class TaskByHubController extends Controller
 {
@@ -18,20 +20,18 @@ class TaskByHubController extends Controller
      * @param PersonalFilterRequest $request
      * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
      */
-    public function busy(PersonalFilterRequest $request)
+    public function busy(PersonalBusyRequest $request)
     {
-        $personal = $this->personal();
+        $personalBusy = $this->personal($request);
 
-        foreach ($request->all() as $key => $filter) {
-            try {
-                $personal->{$key}($filter);
-            } catch (\Exception $e) {
-                report($e);
-            }
-        }
 
-        return PersonalResource::collection($personal->paginate(75))
-            ->additional(['success' => true]);
+        $resource = PersonalBusyResource::collection(collect($personalBusy));
+        $resource->additional([
+            'success' => true,
+            'dates' => $this->getMonthsFromEstimate(),
+        ]);
+
+        return $resource;
     }
 
     /**
@@ -39,26 +39,107 @@ class TaskByHubController extends Controller
      *
      * @return mixed
      */
-    private function personal()
+    private function personal(PersonalBusyRequest $request)
     {
-        $date = new DateTime('-1 month');
+        $personalBusy = [];
+        $personalBusyUsers = [];
 
-        $year = $date->format('Y');
-        $month = $date->format('m');
+        $periodFrom = Carbon::createFromFormat('Y-m-d', $request->year.'-'.$request->month.'-01')->format('Y-m-d');
+        $periodTo = Carbon::createFromFormat('Y-m', $request->year.'-'.$request->month)->endOfMonth()->format('Y-m-d');
+        $dateRange = $this->generateDateRange(Carbon::parse($periodFrom), Carbon::parse($periodTo));
 
-        $personal = Personal::where('is_active', true)
-            ->with(['times' => function ($query) use ($month, $year) {
-                $query->select(DB::raw('sum(worktime) as totaltime'), 'worktime', 'pers_id', 'task_id')
-                    ->whereYear('date', $year)
-                    ->whereMonth('date', $month)
-                    ->groupBy('task_id')
-                    ->groupBy('pers_id');
-            }])->with(['salary' => function ($query) use ($month, $year) {
-                $query->whereYear('date', $year)
-                    ->whereMonth('date', $month)
-                    ->orderBy('date', 'desc');
-            }]);
+        $personals = Personal::select(['pers_id', 'first_name', 'last_name'])
+            ->where('is_active', true)
+            ->with(['personalTasks' => function ($query) use ($periodFrom, $periodTo) {
+                $query->with(['times' => function ($q) {
+                    $q->select(['task_id', 'date', 'worktime']);
+                }]);
+                $query->select(['assignee_id', 'task_id','name', 'estimated_time', 'created_at', 'task_list']);
+                $query->whereIn('task_list', ['In progress', 'Sprint']);
+                $query->where('is_completed', false);
+                $query->whereBetween('created_on',[Carbon::parse($periodFrom)->getTimestamp(), Carbon::parse($periodTo)->getTimestamp()]);
+            }])
+            ->get();
 
-        return $personal;
+        foreach ($dateRange as $date) {
+            foreach ($personals as $key => $personal) {
+                $taskKey = 1;
+                $personalBusyTasks = [];
+
+                foreach ($personal->personalTasks as $task) {
+                        $workTime = collect($task->times)
+                            ->where('date', $date)
+                            ->pluck('worktime')
+                            ->toArray();
+
+                        $dateStart =  collect($task->times)
+                            ->pluck('date')
+                            ->toArray();
+                    
+                        $personalBusyTasks[$taskKey] = [
+                            'name'                   => $task->name,
+                            'task_list'              => $task->task_list,
+                            'estimated_time'         => $task->estimated_time,
+                            'worktime'               => array_sum($workTime),
+                            'different'              => ($workTime) != 0 ? ($task->estimated_time - array_sum($workTime)) : $task->estimated_time - 7,
+                            'date_start'             => !empty($dateStart) ? $dateStart[0] : null,
+                        ];
+                    $taskKey++;
+                    }
+
+                $personalBusyUsers[$key] = [
+                    'first_name'        => $personal->first_name,
+                    'last_name'         => $personal->last_name,
+                    'tasks'             => $personalBusyTasks,
+                ];
+
+            }
+
+            $personalBusy[] = [
+                    'users' => $personalBusyUsers,
+                    'date' => $date,
+                ];
+        }
+
+        return $personalBusy;
+    }
+
+    private function getMonthsFromEstimate()
+    {
+        $dates = [];
+        $months = [];
+        $tasksThisMonth = Task::select('created_at', 'estimated_time')
+        ->whereBetween('created_at', [Carbon::now()->startOfMonth()->format('Y-m-d'), Carbon::now()->endOfMonth()->format('Y-m-d')])
+        ->where('is_completed', false)
+        ->get();
+
+        $maxEstimate = max($tasksThisMonth->pluck('estimated_time')->toArray());
+        $createdAtLongestTask = $tasksThisMonth->where('estimated_time', $maxEstimate)->first()->created_at->format('Y-m-d');
+        $weeksOnLongestTask = round($maxEstimate/7/5);
+        $dateCompleteLongestTask = Carbon::parse($createdAtLongestTask)->addWeeks($weeksOnLongestTask);
+        $rangeDates = $this->generateDateRange(Carbon::now(), $dateCompleteLongestTask);
+
+        foreach ($rangeDates as $key => $date) {
+            $dates[Carbon::parse($date)->format('m')] = Carbon::parse($date)->format('Y');
+        }
+
+        foreach ($dates as $key => $item) {
+            $months[] = [
+                'month' => $key,
+                'year'  => $item,
+            ];
+        }
+
+        return $months;
+    }
+
+
+    private function generateDateRange(Carbon $start_date, Carbon $end_date)
+    {
+        $dates = [];
+        for($date = $start_date; $date->lte($end_date); $date->addDay()) {
+            $dates[] = $date->format('Y-m-d');
+        }
+        return $dates;
     }
 }
